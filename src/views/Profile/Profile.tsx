0c0831cce79aef85b7ffb973d7d14c6ff65c5813
matifandy <matifandy@gmail.com>
2021-06-10 23:42:53 -0300

    profile structure

--- src/views/Profile/Profile.tsx
+++ src/views/Profile/Profile.tsx
@@ -5,14 +5,19 @@ const Profile: React.FC = () => {
   return (
     <div className="profile">
       <div className="profile_top">
+        <div className="profile_image"></div>
+
         <div className="profile_sections">
+          {/* function in every p */}
           <p>My CV</p>
           <p>Applied jobs</p>
           <p>Saved jobs</p>
           <p>Following companies</p>
         </div>
       </div>
-      <div className="section_info"></div>
+      <div className="section_info">
+        {/* This section change with a fuction */}
+      </div>
     </div>
   );
 };
